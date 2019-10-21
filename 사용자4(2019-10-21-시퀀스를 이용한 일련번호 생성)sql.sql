42b3ead029135bd394313e13239dc5f2b721a8b9
hyerinyuu <hyerin.you@gmail.com>
2019-10-21 16:44:09 +0900

    시퀀스를 이용한 일련번호 생성

---
+++ 사용자4(2019-10-21-시퀀스를 이용한 일련번호 생성)sql.sql
@@ -0,0 +1,20 @@
+-- user4 화면
+/*
+새로운 table생성
+이름 : tbl_books
+칼럼 : 코드 : b_code VARCHAR(4)
+       이름 : b_name VARCHAR(50)
+       출판사 : b_comp VARCHAR(50)
+       저자 : b_writer VARCHAR(20)
+       가격 : b_price INT
+*/
+
+CREATE TABLE tbl_books (
+
+    b_code VARCHAR2(4),
+    b_name nVARCHAR2(50),
+    b_comp nVARCHAR2(50),
+    b_writer nVARCHAR2(20),
+    b_price NUMBER
+
+);
